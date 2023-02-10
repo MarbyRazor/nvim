ec0f38d3b5f0c1958c106fe5f1f5ebfcfbf9d94b
MarbyRazor <marby.razor@web.de>
2023-02-10 14:42:42 +0100

    Adding markdown preview

--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -1,13 +1,30 @@
 return function(use)
-  use({
-    "folke/which-key.nvim",
-      config = function()
-        require("which-key").setup({})
-      end
-  })
+  use {
+    'folke/which-key.nvim',
+    config = function()
+      require('which-key').setup {}
+    end,
+  }
 
-  use({
-		"jose-elias-alvarez/null-ls.nvim",
-		"jayp0521/mason-null-ls.nvim",
-	})
+  use {
+    'jose-elias-alvarez/null-ls.nvim',
+    'jayp0521/mason-null-ls.nvim',
+  }
+
+  -- install without yarn or npm
+  use {
+    'iamcco/markdown-preview.nvim',
+    run = function()
+      vim.fn['mkdp#util#install']()
+    end,
+  }
+
+  use {
+    'iamcco/markdown-preview.nvim',
+    run = 'cd app && npm install',
+    setup = function()
+      vim.g.mkdp_filetypes = { 'markdown' }
+    end,
+    ft = { 'markdown' },
+  }
 end
