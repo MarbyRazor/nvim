f6387cd103ec73e117e3cb677d044e93e10b43de
MarbyRazor <marby.razor@web.de>
2023-03-15 10:42:35 +0100

    Clean up for Markdown Preview

--- lua/custom/plugins/markdown-preview.lua
+++ lua/custom/plugins/markdown-preview.lua
@@ -1,12 +1,3 @@
 return {
-    'iamcco/markdown-preview.nvim',
-    run = function()
-      vim.fn['mkdp#util#install']()
-    end,
---     'iamcco/markdown-preview.nvim',
---     run = 'cd app && npm install',
---     setup = function()
---       vim.g.mkdp_filetypes = { 'markdown' }
---     end,
---     ft = { 'markdown' },
+  'iamcco/markdown-preview.nvim',
 }
