4535119360827585290d65ec8f60d3aa44d5d3ce
MarbyRazor <marby.razor@web.de>
2025-02-04 09:04:29 +0100

    Clean up my environment

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,20 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+
+  -- Custom Keymaps
+  vim.keymap.set('i', 'jk', '<ESC>'),
+
+  -- Moving lines
+  vim.keymap.set('n', '<A-j>', ':m .+1<CR>=='),
+  vim.keymap.set('n', '<A-k>', ':m .-2<CR>=='),
+  vim.keymap.set('i', '<A-j>', '<Esc>:m .+1<CR>=='),
+  vim.keymap.set('i', '<A-k>', '<Esc>:m .-2<CR>=='),
+  vim.keymap.set('v', '<A-j>', ":m '>+1<CR>gv=gv"),
+  vim.keymap.set('v', '<A-k>', ":m '<-2<CR>gv=gv"),
+
+  -- Tab visual text
+  vim.keymap.set('v', '<Tab>', '>gv'),
+  vim.keymap.set('v', '<S-Tab>', '<gv'),
+}
