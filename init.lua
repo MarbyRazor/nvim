02670dc9762a940eccb4fed85b64073e11e9ed36
Martin Gerbothe <martin.gerbothe@t-systems.com>
2025-03-17 11:34:01 +0100

    adding yamlls und helm-ls for editing

--- init.lua
+++ init.lua
@@ -687,7 +687,37 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         -- ts_ls = {},
         --
-
+        ['helm-ls'] = {
+          settings = {
+            ['helm-ls'] = {
+              logLevel = 'info',
+              valuesFiles = {
+                mainValuesFile = 'values.yaml',
+                lintOverlayValuesFile = 'values.lint.yaml',
+                additionalValuesFilesGlobPattern = 'values*.yaml',
+              },
+            },
+          },
+        },
+        yamlls = {
+          settings = {
+            yamlls = {
+              enabled = true,
+              enabledForFilesGlob = '*.{yaml,yml}',
+              diagnosticsLimit = 50,
+              showDiagnosticsDirectly = false,
+              path = 'yaml-language-server',
+              config = {
+                schemas = {
+                  kubernetes = 'templates/**',
+                },
+                completion = true,
+                hover = true,
+                -- any other config from https://github.com/redhat-developer/yaml-language-server#language-server-settings
+              },
+            },
+          },
+        },
         lua_ls = {
           -- cmd = { ... },
           -- filetypes = { ... },
@@ -720,6 +750,8 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'markdownlint',
+        'prettier',
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -779,6 +811,9 @@ require('lazy').setup({
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        go = { 'goimports', 'gofmt' },
+        markdown = { 'prettier' },
+        yaml = { 'prettier' },
       },
     },
   },
